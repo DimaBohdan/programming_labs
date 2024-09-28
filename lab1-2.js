67e2fe471f820d9468db6beca3a47182f9081294
DimaBohdan <114583799+DimaBohdan@users.noreply.github.com>
2024-09-28 11:21:07 +0300

    Create lab1-2.js

---
+++ lab1-2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const CountTypes = (argument) => {
+  for (const element of array){
+    const type = typeof element;
+    const count = hash[type] || 0;
+      hash[type] += 1;
+  }
+    return hash;
+};
+
