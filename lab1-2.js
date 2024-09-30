6d91a0696e79046b4f5c961f0f45454ea8552565
DimaBohdan <114583799+DimaBohdan@users.noreply.github.com>
2024-09-30 11:22:34 +0300

    Update lab1-2.js

--- lab1-2.js
+++ lab1-2.js
@@ -1,11 +1,11 @@
 'use strict'
-
-const CountTypes = (argument) => {
+const hash = {};
+const CountTypes = (array) => {
   for (const element of array){
     const type = typeof element;
     const count = hash[type] || 0;
-      hash[type] += 1;
+      hash[type] = count + 1;
   }
     return hash;
 };
-
+console.log(CountTypes([3, 6, 'ddd', true, false]));
