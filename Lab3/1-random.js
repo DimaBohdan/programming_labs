723a2d0e6545b9c4519f1863603e5e6761f14093
DimaBohdan <114583799+DimaBohdan@users.noreply.github.com>
2024-10-19 15:39:25 +0300

    Create 1-random.js

---
+++ Lab3/1-random.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const random = (min, max) => {
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+  const range = max - min + 1;
+  return min + Math.floor(Math.random() * range);
+};
+
+module.exports = { random };
